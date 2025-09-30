5cf6565d8495f03906b649a46225826a16ec6e3c
Frank Dellaert <dellaert@gmail.com>
2025-09-30 17:37:49 -0400

    Robust as well

--- gtsam/nonlinear/NonlinearLikelihood.h
+++ gtsam/nonlinear/NonlinearLikelihood.h
@@ -15,8 +15,8 @@
  **/
 #pragma once
 
-#include <gtsam/nonlinear/PriorFactor.h>
 #include <gtsam/linear/NoiseModel.h>
+#include <gtsam/nonlinear/PriorFactor.h>
 
 #include <optional>
 
@@ -26,142 +26,148 @@ namespace gtsam {
  * A class for a soft prior on any Value type, but with a non-zero mean
  * in the tangent space.
  * The error is `e(x) = Local(origin, x) - mean`.
- * The negative log-likelihood is `0.5 * ||e(x)||^2_Sigma`,
- * where `Sigma` is the covariance from the noise model.
+ * The loss is `0.5 * ||e(x)||^2_Sigma` for a Gaussian noise model,
+ * and `rho(e(x))` for a robust noise model `rho`.
+ * The likelihood is `exp(-loss)`.
  * @ingroup nonlinear
  **/
-  template<class VALUE>
-  class NonlinearLikelihood: public NoiseModelFactorN<VALUE> {
+template <class VALUE>
+class NonlinearLikelihood : public NoiseModelFactorN<VALUE> {
+ public:
+  typedef VALUE T;
 
-  public:
-    typedef VALUE T;
+  // Provide access to the Matrix& version of evaluateError:
+  using NoiseModelFactor1<VALUE>::evaluateError;
 
-    // Provide access to the Matrix& version of evaluateError:
-    using NoiseModelFactor1<VALUE>::evaluateError;
+ private:
+  typedef NoiseModelFactorN<VALUE> Base;
 
+  VALUE origin_; /** The point in the manifold at which the tangent space is
+                    taken. */
+  std::optional<Vector>
+      mean_; /** The mean in the tangent space, default is zero vector. */
 
-  private:
+  /** concept check by type */
+  GTSAM_CONCEPT_TESTABLE_TYPE(T)
 
-    typedef NoiseModelFactorN<VALUE> Base;
+ public:
+  /// Typedef to this class
+  typedef NonlinearLikelihood<VALUE> This;
 
-    VALUE origin_; /** The point in the manifold at which the tangent space is taken. */
-    std::optional<Vector> mean_; /** The mean in the tangent space, default is zero vector. */
+  /// @name Standard Constructors
+  /// @{
 
-    /** concept check by type */
-    GTSAM_CONCEPT_TESTABLE_TYPE(T)
+  /** default constructor - only use for serialization */
+  NonlinearLikelihood() {}
 
-  public:
-    /// Typedef to this class
-    typedef NonlinearLikelihood<VALUE> This;
+  /** Constructor */
+  NonlinearLikelihood(Key key, const VALUE& origin,
+                      const SharedNoiseModel& model,
+                      const std::optional<Vector>& mean = {})
+      : Base(model, key), origin_(origin), mean_(mean) {}
 
-    /// @name Standard Constructors
-    /// @{
+  /// @}
+  /// @name Standard Destructor
+  /// @{
 
-    /** default constructor - only use for serialization */
-    NonlinearLikelihood() {}
+  ~NonlinearLikelihood() override {}
 
-    /** Constructor */
-    NonlinearLikelihood(Key key, const VALUE& origin, const SharedNoiseModel& model,
-      const std::optional<Vector>& mean = {}) :
-      Base(model, key), origin_(origin), mean_(mean) {
+  /// @}
+  /// @name Testable
+  /// @{
+
+  /** print */
+  void print(const std::string& s, const KeyFormatter& keyFormatter =
+                                       DefaultKeyFormatter) const override {
+    std::cout << s << "NonlinearLikelihood on " << keyFormatter(this->key())
+              << "\n";
+    traits<T>::Print(origin_, "  origin: ");
+    if (mean_) {
+      gtsam::print(*mean_, "  tangent space mean: ");
     }
+    if (this->noiseModel_)
+      this->noiseModel_->print("  noise model: ");
+    else
+      std::cout << "no noise model" << std::endl;
+  }
 
-    /// @}
-    /// @name Standard Destructor
-    /// @{
+  /** equals */
+  bool equals(const NonlinearFactor& expected,
+              double tol = 1e-9) const override {
+    const This* e = dynamic_cast<const This*>(&expected);
+    bool mean_equals =
+        (!mean_ && !e->mean_) ||
+        (mean_ && e->mean_ && equal_with_abs_tol(*mean_, *e->mean_, tol));
+    return e != nullptr && Base::equals(*e, tol) &&
+           traits<T>::Equals(origin_, e->origin_, tol) && mean_equals;
+  }
 
-    ~NonlinearLikelihood() override {}
+  /// @}
+  /// @name Factor
+  /// @{
 
-    /// @}
-    /// @name Testable
-    /// @{
+  /// @return a deep copy of this factor
+  gtsam::NonlinearFactor::shared_ptr clone() const override {
+    return std::static_pointer_cast<gtsam::NonlinearFactor>(
+        gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+  }
 
-    /** print */
-    void print(const std::string& s,
-       const KeyFormatter& keyFormatter = DefaultKeyFormatter) const override {
-      std::cout << s << "NonlinearLikelihood on " << keyFormatter(this->key()) << "\n";
-      traits<T>::Print(origin_, "  origin: ");
-      if (mean_) {
-        gtsam::print(*mean_, "  tangent space mean: ");
-      }
-      if (this->noiseModel_)
-        this->noiseModel_->print("  noise model: ");
-      else
-        std::cout << "no noise model" << std::endl;
+  /** vector of errors */
+  Vector evaluateError(const T& x, OptionalMatrixType H) const override {
+    if (H)
+      (*H) = Matrix::Identity(traits<T>::GetDimension(x),
+                              traits<T>::GetDimension(x));
+    // manifold equivalent of (x-z)-mu -> Local(z,x)-mu
+    Vector error = traits<T>::Local(origin_, x);
+    if (mean_) {
+      return error - *mean_;
     }
+    return error;
+  }
 
-    /** equals */
-    bool equals(const NonlinearFactor& expected, double tol=1e-9) const override {
-      const This* e = dynamic_cast<const This*> (&expected);
-      bool mean_equals = (!mean_ && !e->mean_) || (mean_ && e->mean_ && equal_with_abs_tol(*mean_, *e->mean_, tol));
-      return e != nullptr && Base::equals(*e, tol) && traits<T>::Equals(origin_, e->origin_, tol) && mean_equals;
-    }
+  /** Compute the likelihood of a given value */
+  double likelihood(const T& x) const {
+    Vector e = evaluateError(x);
+    double squared_mahalanobis_distance =
+        this->noiseModel_->squaredMahalanobisDistance(e);
+    double loss = this->noiseModel_->loss(squared_mahalanobis_distance);
+    return exp(-loss);
+  }
 
-    /// @}
-    /// @name Factor
-    /// @{
+  /// @}
+  /// @name Access
+  /// @{
 
-    /// @return a deep copy of this factor
-    gtsam::NonlinearFactor::shared_ptr clone() const override {
-      return std::static_pointer_cast<gtsam::NonlinearFactor>(
-          gtsam::NonlinearFactor::shared_ptr(new This(*this))); }
+  const VALUE& origin() const { return origin_; }
+  const std::optional<Vector>& mean() const { return mean_; }
 
-    /** vector of errors */
-    Vector evaluateError(const T& x, OptionalMatrixType H) const override {
-      if (H) (*H) = Matrix::Identity(traits<T>::GetDimension(x),traits<T>::GetDimension(x));
-      // manifold equivalent of (x-z)-mu -> Local(z,x)-mu
-      Vector error = traits<T>::Local(origin_, x);
-      if (mean_) {
-        return error - *mean_;
-      }
-      return error;
-    }
+  /// @}
 
-    /** Compute the likelihood of a given value */
-    double likelihood(const T& x) const {
-      Vector e = evaluateError(x);
-      double squared_error = this->noiseModel_->squaredMahalanobisDistance(e);
-      if (auto gaussian = std::dynamic_pointer_cast<const gtsam::noiseModel::Gaussian>(this->noiseModel_)) {
-        return exp(-0.5 * squared_error - gaussian->negLogConstant());
-      } else {
-        // We don't know the normalization constant, so just return exp(-0.5*error)
-        return exp(-0.5 * squared_error);
-      }
-    }
-
-    /// @}
-    /// @name Access
-    /// @{
-
-    const VALUE & origin() const { return origin_; }
-    const std::optional<Vector>& mean() const { return mean_; }
-
-    /// @}
-
-  private:
-
+ private:
 #if GTSAM_ENABLE_BOOST_SERIALIZATION
-    /** Serialization function */
-    friend class boost::serialization::access;
-    template<class ARCHIVE>
-    void serialize(ARCHIVE & ar, const unsigned int /*version*/) {
-      // NoiseModelFactor1 instead of NoiseModelFactorN for backward compatibility
-      ar & boost::serialization::make_nvp("NoiseModelFactor1",
-          boost::serialization::base_object<Base>(*this));
-      ar & BOOST_SERIALIZATION_NVP(origin_);
-      ar & BOOST_SERIALIZATION_NVP(mean_);
-    }
+  /** Serialization function */
+  friend class boost::serialization::access;
+  template <class ARCHIVE>
+  void serialize(ARCHIVE& ar, const unsigned int /*version*/) {
+    // NoiseModelFactor1 instead of NoiseModelFactorN for backward compatibility
+    ar& boost::serialization::make_nvp(
+        "NoiseModelFactor1", boost::serialization::base_object<Base>(*this));
+    ar& BOOST_SERIALIZATION_NVP(origin_);
+    ar& BOOST_SERIALIZATION_NVP(mean_);
+  }
 #endif
 
-  // Alignment, see https://eigen.tuxfamily.org/dox/group__TopicStructHavingEigenMembers.html
+  // Alignment, see
+  // https://eigen.tuxfamily.org/dox/group__TopicStructHavingEigenMembers.html
   inline constexpr static auto NeedsToAlign = (sizeof(T) % 16) == 0;
-  public:
+
+ public:
   GTSAM_MAKE_ALIGNED_OPERATOR_NEW_IF(NeedsToAlign)
-  };
+};
 
-  /// traits
-  template<class VALUE>
-  struct traits<NonlinearLikelihood<VALUE> > : public Testable<NonlinearLikelihood<VALUE> > {};
+/// traits
+template <class VALUE>
+struct traits<NonlinearLikelihood<VALUE> >
+    : public Testable<NonlinearLikelihood<VALUE> > {};
 
-
-} /// namespace gtsam
+}  // namespace gtsam
--- gtsam/nonlinear/tests/testNonlinearLikelihood.cpp
+++ gtsam/nonlinear/tests/testNonlinearLikelihood.cpp
@@ -15,13 +15,13 @@
  * @brief unit tests for NonlinearLikelihood factor
  */
 
-#include <gtsam/nonlinear/NonlinearLikelihood.h>
-#include <gtsam/geometry/Pose2.h>
+#include <CppUnitLite/TestHarness.h>
 #include <gtsam/base/Testable.h>
 #include <gtsam/base/numericalDerivative.h>
-#include <CppUnitLite/TestHarness.h>
+#include <gtsam/geometry/Pose2.h>
+#include <gtsam/nonlinear/NonlinearLikelihood.h>
 
-using namespace std; 
+using namespace std;
 using namespace gtsam;
 
 //******************************************************************************
@@ -49,7 +49,8 @@ TEST(NonlinearLikelihood, Error) {
   NonlinearLikelihood<Pose2> factor(key, origin, model);
 
   Pose2 x(1.1, 2.2, 0.3);
-  Vector expected_error = (Vector(3) << 0.15463769, 0.161515277, 0.0).finished();
+  Vector expected_error =
+      (Vector(3) << 0.15463769, 0.161515277, 0.0).finished();
   Vector actual_error = factor.evaluateError(x);
   EXPECT(assert_equal(expected_error, actual_error, 1e-8));
 }
@@ -63,7 +64,8 @@ TEST(NonlinearLikelihood, ErrorWithMean) {
   NonlinearLikelihood<Pose2> factor(key, origin, model, mean);
 
   Pose2 x(1.1, 2.2, 0.3);
-  Vector expected_error = (Vector(3) << 0.05463769, -0.038484723, -0.05).finished();
+  Vector expected_error =
+      (Vector(3) << 0.05463769, -0.038484723, -0.05).finished();
   Vector actual_error = factor.evaluateError(x);
   EXPECT(assert_equal(expected_error, actual_error, 1e-8));
 }
@@ -77,7 +79,7 @@ TEST(NonlinearLikelihood, Likelihood) {
   NonlinearLikelihood<Pose2> factor(key, origin, model, mean);
 
   Pose2 x = origin.retract(mean);
-  double expected_likelihood = 1.0 / sqrt(pow(2 * M_PI, 3));
+  double expected_likelihood = 1.0;
   double actual_likelihood = factor.likelihood(x);
   EXPECT_DOUBLES_EQUAL(expected_likelihood, actual_likelihood, 1e-9);
 }
